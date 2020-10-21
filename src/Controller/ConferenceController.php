837fa3e82f788b25cf3301a909052b201bd09dfd
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2020-10-21 10:12:27 -0400

    replace id by slug and create comment form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,12 +17,13 @@ use Twig\Environment;
 class ConferenceController extends AbstractController
 {
     private $twig;
+    private $entityManager;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
-
 
     /**
      * @Route("/", name="homepage")
@@ -32,13 +36,25 @@ class ConferenceController extends AbstractController
     }
 
     /**
-     * @Route("/conference/{id}", name="conference")
+     * @Route("/conference/{slug}", name="conference")
      */
     public function show(
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository
     ) {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment, ['allow_extra_fields' => true]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference',['slug' => $conference->getSlug()]);
+        }
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return new Response(
@@ -48,7 +64,8 @@ class ConferenceController extends AbstractController
                     'conference' => $conference,
                     'comments' => $paginator,
                     'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-                    'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+                    'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+                    'comment_form' => $form->createView()
                 ]
             )
         );
